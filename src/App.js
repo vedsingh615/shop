ab4ba308aaa79ab52da94ff8cb6c3690dc88bab1
unknown <you@example.com>
2025-07-12 13:42:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Hero from './components/hero';
 function App() {
   return (
     <div className="App">
     <h1>google</h1>
+    <Hero/>
     </div>
   );
 }
