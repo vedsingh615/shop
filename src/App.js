9d4e804495362ab34f582cf18af681a698126721
Ajay Kumar <ajaykumar8752124@gmail.com>
2025-07-12 14:27:11 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Hero from './components/hero';
+import Navbar from './components/navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
     <h1>google</h1>
     <Hero/>
+
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='app'>
+ Navbar
+    </div>
+  )
+}
+
+export default navbar
