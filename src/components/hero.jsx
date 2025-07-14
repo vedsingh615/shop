1a16a4be706ca2923ecce15f918208c67a916d26
Ajay Kumar <ajaykumar8752124@gmail.com>
2025-07-14 12:46:51 +0530

    update

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,13 +6,13 @@ function hero() {
 <div id="carouselExampleInterval" className="carousel slide slide " data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active" data-bs-interval="10000">
-      <img src="https://petapixel.com/assets/uploads/2017/03/product1.jpeg" className="d-block w-100" alt="..."/>
+      <img src="https://petapixel.com/assets/uploads/2017/03/product1.jpeg" className="d-block w-100" alt="..." style={{width:'90%',height:'50vh'}}/>
     </div>
     <div className="carousel-item" data-bs-interval="2000">
-      <img src="https://fashion-slider.uiinitiative.com/images/yeeze.jpg" className="d-block w-100" alt="..."/>
+      <img src="https://fashion-slider.uiinitiative.com/images/yeeze.jpg" className="d-block w-100" alt="..." style={{width:'90%',height:'50vh'}}/>
     </div>
     <div className="carousel-item">
-      <img src="https://fashion-slider.uiinitiative.com/images/nike.jpg" className="d-block w-100" alt="..."/>
+      <img src="https://fashion-slider.uiinitiative.com/images/nike.jpg" className="d-block w-100" alt="..." style={{width:'90%',height:'50vh'}}/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
