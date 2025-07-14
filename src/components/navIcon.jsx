9b8d056a8f5097a4eef2c9632cf7645ebeacd26b
Ajay Kumar <ajaykumar8752124@gmail.com>
2025-07-14 14:57:09 +0530

    update

--- src/components/navIcon.jsx
+++ src/components/navIcon.jsx
@@ -5,43 +5,44 @@ function NavIcon() {
     <div className='App'>
 <div className="grid-container" style={{width:'90%',left:'80px'}}>
    <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Camera</p>
+      <img src='https://img.tatacliq.com/images/i18//437Wx649H/MP000000022475794_437Wx649H_202406031854361.jpeg' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>T-Shirts</p>
     </div>
       <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Clothes</p>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzfIYT8J0oM--7sUlUNim-6WatpOheF8ShYl9Kpw2-VvKh_aVAtV3I8ydAprIRzPXnAb4&usqp=CAU' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>Caps</p>
+    </div>
+      <div className="grid-item">
+      <img src='https://m.media-amazon.com/images/I/61hv49VjMuL._AC_UL320_.jpg' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>Shirts</p>
+    </div> 
+   
+      <div className="grid-item">
+      <img src='https://m.media-amazon.com/images/I/51Z7bpvdeAL._AC_UL640_QL65_.jpg' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>Shoes</p>
+    </div> <div className="grid-item">
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlF-jGp3vAZGpvNq8RC_dEA_jj7ccSei5Daw&s' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>Cargos</p>
+    </div>
+      <div className="grid-item">
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVgupZrmPLHgzyzUFJF6__fqHepV_TX-_1NxEQ7s3h-Ys2KHn9uMpM8IPJf7o98ZEIXUw&usqp=CAU' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>Jackets</p>
+    </div> <div className="grid-item">
+      <img src='https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTTZ1bQuubs2e6N4Z5MODoSlLN-SSInpGJ3Ro1-ndQFNlLhXFKVfwOclGPznzjJtXpz0FhPRtM7SUKsk9abfTMXYy8_yD53oaWhmaIVtv79UKHIvM1BLhnSSw' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>Jeans</p>
     </div>
     <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Camera</p>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-TbYx7SsnOvRIOMGuSXVvWCb2yghUwFc6tNWvjdTKrsGzg2Kkl3KZPTvbDq4B8iEvAcc&usqp=CAU' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>Watches</p>
     </div>
       <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Camera</p>
-    </div> <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Camera</p>
+      <img src='https://m.media-amazon.com/images/I/31NRzJMhp9L._UY1100_.jpg' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>Bagpacks</p>
     </div>
       <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Camera</p>
-    </div> <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Camera</p>
+      <img src='https://img.freepik.com/premium-psd/perfume-bottle-sand_23-2148961294.jpg?w=740' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
+      <p>Perfume</p>
     </div>
-      <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Camera</p>
-    </div> <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Camera</p>
-    </div>
-      <div className="grid-item">
-      <img src='https://img.freepik.com/premium-photo/dslr-camera-with-flash_144962-4377.jpg?w=826' style={{height:"80px",width:"70px",mixBlendMode:"multiply"}} alt='cam'/>
-      <p>Camera</p>
-    </div>
-     
     
 </div>
 
