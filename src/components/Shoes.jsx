cd2a7efb8bd5682b765094470658b635dc00ded3
Ajay Kumar <ajaykumar8752124@gmail.com>
2025-07-17 12:03:27 +0530

    update

--- src/components/Shoes.jsx
+++ src/components/Shoes.jsx
@@ -2,46 +2,46 @@ import React from 'react';
 import ProductCard from './ProductCard';
 
 const Shoes = () => {
-  const ShoesImage = "https://m.media-amazon.com/images/I/51Z7bpvdeAL._AC_UL640_QL65_.jpg";
+
 
   const tshirtProducts = [
     {
-      image: ShoesImage,
+      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_0-woY-VnUVNXdXmcgaB12m_0i-ew-T2J-Q&s',
       title: 'Black Graphic Tee',
       category: 'T-Shirts',
       price: 799,
       href: '/product/tshirt-black',
     },
     {
-      image: ShoesImage,
+      image:'https://m.media-amazon.com/images/I/61pzvCOBFQL._AC_UL320_.jpg',
       title: 'Marvel Logo Tee',
       category: 'T-Shirts',
       price: 899,
       href: '/product/tshirt-marvel',
     },
     {
-      image: ShoesImage,
+      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSR2Q53RFM9TD1E6DOjw-NYOSbmTKoHb5uTw&s',
       title: 'Minimal White Tee',
       category: 'T-Shirts',
       price: 699,
       href: '/product/tshirt-white',
     },
     {
-      image: ShoesImage,
+      image:'https://m.media-amazon.com/images/I/61wYbVMG+-L._AC_UL320_.jpg',
       title: 'Anime Print Oversized Tee',
       category: 'T-Shirts',
       price: 999,
       href: '/product/tshirt-anime',
     },
     {
-      image: ShoesImage,
+      image:'https://m.media-amazon.com/images/I/71GqjRjvRoL._AC_UL320_.jpg',
       title: 'Classic Navy T-Shirt',
       category: 'T-Shirts',
       price: 749,
       href: '/product/tshirt-navy',
     },
     {
-      image: ShoesImage,
+      image:'https://m.media-amazon.com/images/I/51zJY0bS3-L._AC_UL320_.jpg',
       title: 'Slogan Printed Tee',
       category: 'T-Shirts',
       price: 899,
@@ -49,28 +49,28 @@ const Shoes = () => {
     },
     
     {
-      image: ShoesImage,
+      image:'https://m.media-amazon.com/images/I/71lPjDHvjtL._AC_UL320_.jpg',
       title: 'Minimal White Tee',
       category: 'T-Shirts',
       price: 699,
       href: '/product/tshirt-white',
     },
     {
-      image: ShoesImage,
+      image:'https://m.media-amazon.com/images/I/713C4wFx0jL._AC_UL320_.jpg',
       title: 'Anime Print Oversized Tee',
       category: 'T-Shirts',
       price: 999,
       href: '/product/tshirt-anime',
     },
     {
-      image: ShoesImage,
+      image:'https://m.media-amazon.com/images/I/61PZyk7PN-L._AC_UL320_.jpg',
       title: 'Classic Navy T-Shirt',
       category: 'T-Shirts',
       price: 749,
       href: '/product/tshirt-navy',
     },
     {
-      image: ShoesImage,
+      image:'https://m.media-amazon.com/images/I/51vQbuuO2eL._AC_UL320_.jpg',
       title: 'Slogan Printed Tee',
       category: 'T-Shirts',
       price: 899,
