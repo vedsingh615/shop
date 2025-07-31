788a7a24e9becb35d3083d622c09946a2424073c
unknown <you@example.com>
2025-07-31 20:36:16 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/HF/Layout';
-
 import './App.css';
 
 import Login from './components/form/Login';
 import Register from './components/form/Register';
-import Tshirt from './components/pages/Tshirt';
-import Caps from './components/pages/Caps';
-import Jackets from './components/pages/Jackets';
-import Cargos from './components/pages/Cargos';
-import Shoes from './components/pages/Shoes';
-import Shirts from './components/pages/Shirts';
-import Bagpacks from './components/pages/Bagpacks';
-import Jeans from './components/pages/Jeans'; 
-import Perfumes from './components/pages/Perfumes';
-import Watches from './components/pages/Watches';
-import Navicon from './components/navIcon';
-import Hero from './components/hero';
+import Hero from './components/HF/hero';
+import AllClothes from './components/pages/AllClothes';
 
 
 
@@ -30,20 +19,11 @@ const router = createBrowserRouter([
     children: [
       {
       path: '/',
-      element: <><Navicon /><Hero/></>
+      element: <><Hero/><AllClothes/></>
   },   
   {path: '/Login', element:    <Login /> },
   {path: '/Register', element: <Register /> },
-  {path: '/tshirts', element:   <Tshirt />},
-  {path: '/caps', element:  < Caps/> },
-  {path: '/jackets', element:  <Jackets /> },
-  {path: '/cargos', element:  <Cargos />},
-  {path: '/shirts', element: <Shirts /> },
-  {path: '/shoes', element: <Shoes /> },
-  {path: '/bagpacks', element:  <Bagpacks /> },
-  {path: '/jeans', element:  <Jeans /> },
-  {path: '/perfumes', element: <Perfumes /> },
-  {path: '/watches', element:  <Watches /> },
+  
 ],
 },
 ]);
